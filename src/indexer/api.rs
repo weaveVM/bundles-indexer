564c8282ebdfeb3a861a7ecf4f4ca963f2db178c
charmful0x <charmful0x@gmail.com>
2025-02-22 16:28:48 +0100

    feat: resolve envelope

--- src/indexer/api.rs
+++ src/indexer/api.rs
@@ -1,6 +1,8 @@
 use crate::indexer::cloud::get_bundle_by_envelope;
 use crate::utils::bundles::get_envelope_from_bundle;
+use axum::response::IntoResponse;
 use axum::{extract::Path, Json};
+use reqwest::{header, StatusCode};
 use serde_json::{json, Value};
 
 pub async fn get_root() -> Json<Value> {
@@ -15,4 +17,67 @@ pub async fn get_envelope_raw(Path(envelope_txid): Path<String>) -> Json<Value> 
         .await
         .unwrap();
     Json(serde_json::to_value(&envelope).unwrap())
+}
+
+pub async fn resolve_envelope(Path(envelope_txid): Path<String>) -> impl IntoResponse {
+    let bundle_txid = get_bundle_by_envelope(&envelope_txid)
+        .await
+        .unwrap_or_default();
+
+    if bundle_txid.len() == 0 {
+        return (
+            StatusCode::NOT_FOUND,
+            Json(json!({"error": "envelope index out of range"})),
+        )
+            .into_response();
+    }
+
+    let envelope = get_envelope_from_bundle(&bundle_txid, &envelope_txid)
+        .await
+        .unwrap();
+
+    let input: String = match Some(envelope.clone().input) {
+        Some(input) => input,
+        None => {
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(json!({ "error": "No input data found in envelope" })),
+            )
+                .into_response()
+        }
+    };
+
+    let content_type = envelope
+        .tags
+        .clone()
+        .map(|tags| {
+            tags.iter()
+                .find(|tag| tag.name.to_lowercase() == "content-type")
+                .map(|tag| tag.value.clone())
+        })
+        .flatten()
+        .unwrap_or_else(|| "application/octet-stream".to_string());
+
+    let processed_data = match hex::decode(input.trim_start_matches("0x")) {
+        Ok(data) => data,
+        Err(_) => {
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(json!({ "error": "Failed to process input data" })),
+            )
+                .into_response()
+        }
+    };
+
+    (
+        [
+            (header::CONTENT_TYPE, content_type),
+            (
+                header::CACHE_CONTROL,
+                "public, max-age=31536000".to_string(),
+            ),
+        ],
+        processed_data,
+    )
+        .into_response()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 pub mod indexer;
 pub mod utils;
 
-use crate::indexer::api::{get_envelope_raw, get_root};
+use crate::indexer::api::{get_envelope_raw, get_root, resolve_envelope};
 use crate::indexer::cronjob::index;
 use crate::utils::rpc::init_wvm_rpc;
 
 use axum::{routing::get, Router};
-use tokio::task;
 
 #[shuttle_runtime::main]
 async fn main(
@@ -23,13 +22,14 @@ async fn main(
     // server routes
     let router = Router::new()
         .route("/", get(get_root))
-        .route("/envelope/:txid", get(get_envelope_raw));
+        .route("/envelope/:txid", get(get_envelope_raw))
+        .route("/resolve/:txid", get(resolve_envelope));
 
-    // task::spawn(async move {
-    //     loop {
-    //         index(provider.clone()).await.unwrap();
-    //     }
-    // });
+    tokio::task::spawn(async move {
+        loop {
+            index(provider.clone()).await.unwrap();
+        }
+    });
 
     Ok(router.into())
 }
